37fa81ae3d8e0767de55d34b66c0bba909169380
Dragomegak <stevenle6569@gmail.com>
2020-05-17 11:33:21 -0400

    shows github api elements

--- src/App.js
+++ src/App.js
@@ -4,35 +4,22 @@ import './App.css';
 
 class App extends Component {
   state = {
-      data: null
+      loading: true
+      
     };
   
-    componentDidMount() {
+    async componentDidMount() {
         // Call our fetch function below once the component mounts
-      this.callBackendAPI()
-        .then(res => this.setState({ data: res.express }))
-        .catch(err => console.log(err));
+        const url = "https://api.github.com/users/dragomegak/repos";
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
     }
-      // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
-    callBackendAPI = async () => {
-      const response = await fetch('/express_backend');
-      const body = await response.json();
-  
-      if (response.status !== 200) {
-        throw Error(body.message) 
-      }
-      return body;
-    };
-  
     render() {
       return (
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
+        <div>
           // Render the newly fetched data inside of this.state.data 
-          <p className="App-intro">{this.state.data}</p>
+          {this.state.loading ? <div>loading...</div> : <div>person...</div>}
         </div>
       );
     }
