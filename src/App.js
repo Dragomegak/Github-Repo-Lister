63a5dcebf68b2374508df0fbcb40c8766eb36fff
Dragomegak <stevenle6569@gmail.com>
2020-05-17 12:00:06 -0400

    Added push to states for all project names

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   state = {
-      loading: true
-      
+      loading: true,
+      projectName: [],
     };
   
     async componentDidMount() {
@@ -13,13 +12,25 @@ class App extends Component {
         const url = "https://api.github.com/users/dragomegak/repos";
         const response = await fetch(url);
         const data = await response.json();
+        var i;
+        for (i = 0; i < data.length; i++){
+          this.state.projectName.push({projectName: data[i].name});
+        }
+        console.log(this.state.projectName)
+        /* this.setState({projectName: data.results});
         console.log(data);
+        console.log(data.length) */
+        /* console.log(data[0].name); */
     }
     render() {
       return (
         <div>
-          // Render the newly fetched data inside of this.state.data 
-          {this.state.loading ? <div>loading...</div> : <div>person...</div>}
+          {/* Render the newly fetched data inside of this.state.data */} 
+          {this.state.loading || !this.state.projectName ? (
+            <div>loading...</div>
+            ) : (
+            <div><div>{this.state.projectName}</div></div>
+          )}
         </div>
       );
     }
