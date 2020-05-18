81d11a248a4c298d8145eab3a3efe42fb433aff9
Dragomegak <stevenle6569@gmail.com>
2020-05-18 18:01:45 -0400

    Added an async function to try to extract and list the tech used in a project

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ class App extends Component {
       projectName: [],
       projectDescription: [],
       projectUrl: [],
-      techUsed: [],
+      techUsedUrl: [],
       lastUpdated: [],
+      techUsed: [],
     };
   
     async componentDidMount() {
@@ -16,26 +17,42 @@ class App extends Component {
         const url = "https://api.github.com/users/dragomegak/repos";
         const response = await fetch(url);
         const data = await response.json();
-        var i;
+        let i = 0;
         for (i = 0; i < data.length; i++){
           this.state.projectName.push({projectName: data[i].name});
           this.state.projectDescription.push({projectDescription: data[i].description});
           this.state.projectUrl.push({projectUrl: data[i].html_url});
           this.state.lastUpdated.push({lastUpdated: data[i].updated_at});
-          this.state.techUsed.push({techUsed: data[i].language});
+          this.state.techUsedUrl.push({techUsedUrl: data[i].languages_url});
           /* use languages_url for more details on languages (in object), use language for large makup */
           /* need to find a way to fetch second link: languages used and only the key */
         }
-        console.log(this.state.projectName)
+        /* console.log(this.state.projectName)
         console.log(this.state.projectDescription)
         console.log(this.state.projectUrl)
         console.log(this.state.lastUpdated)
-        console.log(this.state.techUsed)
-        
-        /* this.setState({projectName: data.results});
+        console.log(this.state.techUsed) 
+        this.setState({projectName: data.results});
         console.log(data);
-        console.log(data.length) */
-        /* console.log(data[0].name); */
+        console.log(data.length) 
+        console.log(data[0].name); */
+        this.techUsedList();
+    }
+    techUsedList = async () =>{
+      let i = 0;
+      for (i = 0; i < this.state.techUsedUrl.length; i++){
+        const url = this.state.techUsedUrl[i];
+        const response = await fetch(url);
+        const data = await response.json();
+        let j = 0;
+        let tempList = [];
+        for (j = 0; j < data.length; j++){
+          tempList.push(data[i]);
+          console.log(data[i]);
+        }
+        this.state.techUsed.push(tempList);
+        console.log(this.state.techUsed)
+      }
     }
     render() {
       return (
